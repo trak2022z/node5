74f5851689c7e2a65a7fcb9e5c3eac5209b7c522
Tom Krok <91782249+trak2022z@users.noreply.github.com>
2022-04-04 08:36:40 +0000

    2

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-//Code Quality with Node.js
-/**
- * A eager but inexperienced CSE 154 student wrote the code below as a start to their cool
- * web service. However, there are many code quality issues, see if you can spot them all
+/*
+  solution code
+  NOTE: this solution lacks documentation which is an essential part of code quality
+
+  List of issues in provided code:
+  - missing 'use strict'
+  - using `app` but it's undefined. Need to require express and create an instance of the express object
+  - all helper functions should be defined below endpoint definitions
+  - endpoint /trial1/:mystery
+    - should be ===
+    - should be res.type not req.type
+    - if the param has a value of spooky, no response is ever sent (left hanging)
+    - overwrites type by initially setting type to text and then setting to json
+  - endpoint /trial2
+    - no type is ever set
+    - in the case of an error, status code is never set
+    - passes `res` to helper function
+    - structure of endpoint is faulty because `.send` is called more than once
+  - endpoint /trial1/:mystery2
+    - response never sent because path is the same as first endpoint definition in file
+  - missing app.use(express.static('public'));
+  - does not listen at a designated port
+  - missing documentation
+    - header comment should include documentation for each endpoint in addition to a file description
+    - brief comment above each endpoint
+    - all functions documented using JSDoc convention
 */
 
 app.get('/trial1/:mystery', function(req, res) {
